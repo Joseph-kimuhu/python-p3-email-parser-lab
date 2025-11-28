3900cb786703c7f8eeb236bedd6cfb380adf976d
Joseph-Kimuhu <joseph.kimuhu@student.moringaschool.com>
2025-11-28 10:21:24 +0300

    first commit

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,10 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email_string = email_string
+    
+    def parse(self):
+        tokens = re.split(r'[,\s]+', self.email_string)
+        emails = [token for token in tokens if re.match(r'^[^@]+@[^@]+\.[^@]+$', token)]
+        return sorted(list(set(emails)))
--- lib/testing/email_address_parser_test.py
+++ lib/testing/email_address_parser_test.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from email_address_parser import EmailAddressParser
 
 class TestEmailAddressParser:
